e11716d9737fc4a4a7d5d854f0e2f15e2277dce8
Alisson Goulart <alisson.goulart@fitcard.com.br>
2023-04-11 15:59:10 -0300

    Create Dashboard Page

---
+++ src/components/Layout/index.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native';
+
+export const Row = styled.View`
+  flex-direction: row;
+  align-items: flex-start;
+`;
+
+export const Section = styled.View`
+  margin-top: 30px;
+  margin-bottom: 20px;
+`;
+
+export const CenteredContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+import metrics from "../../styles/metrics";
+
+export default StyleSheet.create({
+  safeAreaView: {
+    backgroundColor: '#232323',
+    flex: 1,
+    padding: 6,
+  },
+});
